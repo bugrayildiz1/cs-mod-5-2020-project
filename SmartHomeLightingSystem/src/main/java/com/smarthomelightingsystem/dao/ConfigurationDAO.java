247239d300e4710e6c991e1852a0292436610fe0
s2207966 <a.shynkar@student.utwente.nl>
2020-10-08 14:55:24 +0200

    created new DAO files

---
+++ SmartHomeLightingSystem/src/main/java/com/smarthomelightingsystem/dao/ConfigurationDAO.java
@@ -0,0 +1,18 @@
+package com.smarthomelightingsystem.dao;
+/**
+ * The class works with animations of LED strip
+ * 
+ * @author Jasper van Amerongen
+ * @author Albina Shynkar
+ * @author Lola Solovyeva
+ * @author Ilya Averchenko
+ * @author Bugra Yildiz
+ * @author Alexandru Matcov
+
+ * 
+ * @version 1
+ *
+ */
+public class ConfigurationDAO {
+
+}
---
+++ SmartHomeLightingSystem/src/main/java/com/smarthomelightingsystem/dao/PresetDAO.java
@@ -0,0 +1,17 @@
+package com.smarthomelightingsystem.dao;
+/**
+ * The class works with presets for LED strip
+ * 
+ * @author Jasper van Amerongen
+ * @author Albina Shynkar
+ * @author Lola Solovyeva
+ * @author Ilya Averchenko
+ * @author Bugra Yildiz
+ * @author Alexandru Matcov
+ * 
+ * @version 1
+ *
+ */
+public class PresetDAO {
+
+}

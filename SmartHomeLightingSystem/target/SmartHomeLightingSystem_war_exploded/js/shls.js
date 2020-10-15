58e0fa1bea1b3ead638eed61c69337d6b2aab9db
jvanamerongen2002 <57096003+jvanamerongen2002@users.noreply.github.com>
2020-10-15 16:21:37 +0200

    Made UI structure and Pallate page responsive + started work on the welcome pages

--- SmartHomeLightingSystem/target/SmartHomeLightingSystem_war_exploded/js/shls.js
+++ SmartHomeLightingSystem/target/SmartHomeLightingSystem_war_exploded/js/shls.js
@@ -1,3 +1,5 @@
+let selColor = "#FFFFFF";
+
 $(document).ready(function() {
 
     // Start MDC and set up components
@@ -6,12 +8,13 @@ $(document).ready(function() {
     document.querySelector(".shls-pallate-functions-tabbar").MDCTabBar.activateTab(0);
 
     // Render screen and open first page
-    renderColorWheel();
     openPallate();
     openPallateAnimations();
 
     // Only when screen is ready
     renderSwiper();
+    onResize();
+    $(window).resize(onResize);
     const slider = new mdc.slider.MDCSlider(document.querySelector('.mdc-slider')); //must be after openPallate
 
 });
@@ -58,14 +61,17 @@ function openPallatePresets() {
 
 }
 
-function renderColorWheel() {
+function renderColorWheel(size) {
 
     const rootEl = ".shls-pallate-iro-colorwheel";
     const shadowId = "shls-pallate-iro-colorwheel-shadow";
 
+    $(rootEl).empty().width(size + "px");
+
     // Initiate iro.js color wheel
     const wheel = new iro.ColorPicker(rootEl, {
-        width: 250,
+        width: size,
+        color: selColor,
         layout: [ { component: iro.ui.Wheel } ],
         handleSvg: "#shls-pallate-iro-colorwheel-handle",
         borderColor: "#fff",
@@ -87,31 +93,55 @@ function renderColorWheel() {
     // Add listener and change theme accordingly
     wheel.on('color:change', (color) => {
 
-        const rgb = parseInt(color.hexString.substring(1), 16);   // convert rrggbb to decimal
+        selColor = color.hexString;
+        const rgb = parseInt(selColor.substring(1), 16);   // convert rrggbb to decimal
         const r = (rgb >> 16) & 0xff;  // extract red
         const g = (rgb >>  8) & 0xff;  // extract green
         const b = (rgb >>  0) & 0xff;  // extract blue
         const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
 
-        if (luma < 200) $("html").attr("style","--mdc-theme-secondary: " + color.hexString);
-        else $("html").attr("style","--mdc-theme-secondary: var(--mdc-theme-primary)");
+        if (luma < 200) $(":root").css("--mdc-theme-secondary", selColor);
+        else $(":root").css("--mdc-theme-secondary", "var(--mdc-theme-primary)");
+
+        // SEND TO BE
 
     });
 
 }
 
+function onResize() {
+
+    let w = $(window).width();
+    let colorWheelWidth;
+
+    if (w < 600) colorWheelWidth = 250;
+    else if (w >= 600 && w < 900) colorWheelWidth = 280;
+    else if (w > 900) colorWheelWidth = 300;
+
+    if ($(".shls-pallate-iro-colorwheel").width() != colorWheelWidth) renderColorWheel(colorWheelWidth);
+
+    if (w < 840) $(":root").css("--shls-mdc-gutter", "16px");
+    else $(":root").css("--shls-mdc-gutter", "24px");
+
+}
+
 function renderSwiper() {
 
-    new Swiper('.swiper-container', {
+    new Swiper(".swiper-container", {
         slidesPerView: 3,
-        spaceBetween: 20,
         centeredSlides: true,
         slideToClickedSlide: true,
         pagination: {
-            el: '.swiper-pagination',
+            el: ".swiper-pagination",
             dynamicBullets: true,
             clickable: true
         },
+        breakpoints: {
+            1000: { slidesPerView: 7 },
+            840: { slidesPerView: 6 },
+            700: { slidesPerView: 5 },
+            600: { slidesPerView: 4 }
+        }
     });
 
 }

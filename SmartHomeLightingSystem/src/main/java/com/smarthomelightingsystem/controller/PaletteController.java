81843b6130c9c1a1cafc38e7a3828f1e3a983438
jvanamerongen2002 <57096003+jvanamerongen2002@users.noreply.github.com>
2020-10-27 12:08:41 +0100

    [DUPE] Build PallatePage (Animations + Presets) data chain (controller, models and DAO). Same for LDRData, but the DAO is incomplete; waiting for DB!

--- SmartHomeLightingSystem/src/main/java/com/smarthomelightingsystem/controller/PaletteController.java
+++ SmartHomeLightingSystem/src/main/java/com/smarthomelightingsystem/controller/PaletteController.java
@@ -21,6 +21,7 @@ public class PaletteController {
         pp.setAnimations(new AnimationDAO().getAll());
         pp.setPresets(new PresetDAO().getAll());
         return pp;
+        // test
 
     }
 

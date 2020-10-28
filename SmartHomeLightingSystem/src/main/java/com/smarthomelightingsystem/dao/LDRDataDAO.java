888b4510ac9a3b327ff45f046e052bd664b231c5
jvanamerongen2002 <57096003+jvanamerongen2002@users.noreply.github.com>
2020-10-28 14:08:29 +0100

    Added comments to LDRDataDAO.java

--- SmartHomeLightingSystem/src/main/java/com/smarthomelightingsystem/dao/LDRDataDAO.java
+++ SmartHomeLightingSystem/src/main/java/com/smarthomelightingsystem/dao/LDRDataDAO.java
@@ -4,19 +4,19 @@ import com.smarthomelightingsystem.model.LDRData;
 
 public class LDRDataDAO {
 
-    public LDRData getDefault(boolean mobile) {
+    public LDRData getDefault(boolean mobile) { // last 8h | mobile false: every h; mobile true: every 2h
         return null;
     }
 
-    public LDRData getDay(boolean mobile) {
+    public LDRData getDay(boolean mobile) { // mobile false: every 2h; mobile true: every 4h
         return null;
     }
 
-    public LDRData getWeek(boolean mobile) {
+    public LDRData getWeek() { // every day regardless of mobile
         return null;
     }
 
-    public LDRData getMonth(boolean mobile) {
+    public LDRData getMonth(boolean mobile) { // mobile false: every 2d; mobile true: every 4d
 
         return null;
 

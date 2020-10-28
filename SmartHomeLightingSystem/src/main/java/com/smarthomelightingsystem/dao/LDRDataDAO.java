17ae0c1aa7862df9aa51514364a5efc7af1d55f8
s2207966 <a.shynkar@student.utwente.nl>
2020-10-28 17:27:30 +0100

    some changes LDRDataDAO,LDRDataController

--- SmartHomeLightingSystem/src/main/java/com/smarthomelightingsystem/dao/LDRDataDAO.java
+++ SmartHomeLightingSystem/src/main/java/com/smarthomelightingsystem/dao/LDRDataDAO.java
@@ -1,10 +1,31 @@
 package com.smarthomelightingsystem.dao;
 
+
+
+import java.sql.PreparedStatement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import com.smarthomelightingsystem.data.Database;
 import com.smarthomelightingsystem.model.LDRData;
 
 public class LDRDataDAO {
+	private static final String TABLENAME = "ldr_data";
 
     public LDRData getDefault(boolean mobile) { // last 8h | mobile false: every h; mobile true: every 2h
+    	Database DB = new Database();
+    	try {
+    		if (mobile == true) {
+    			String q = "SELECT * "
+    			+ "FROM ldr_data_day "
+    			+ "WHERE ";
+    		}else if(mobile!= true) {
+    			String q= "SELECT * FROM ldr_data_day;";
+    		}
+    	}catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			DB.close();
+		}
         return null;
     }
 
@@ -23,6 +44,22 @@ public class LDRDataDAO {
     }
 
     public void setValue(float value) {
+    	Database DB = new Database();
+    	try {
+    		String q = "INSERT INTO " + TABLENAME + "(data, timestamp) VALUES(?, ?);" ;
+    		PreparedStatement prepStatement = DB.connection.prepareStatement(q);
+			prepStatement.setFloat(1, value);
+			//LocalDateTime localDateTime = m.getTime().toInstant().atZone(ZoneId.of("GMT+0")).toLocalDateTime();
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+			LocalDateTime now = LocalDateTime.now(); 
+			prepStatement.setObject(2, dtf.format(now));
+			
+			DB.executePreparedStatement(prepStatement);
+    	}catch (Exception e) {
+    		e.printStackTrace();
+    	}finally{
+			DB.close();
+		}
 
     }
 

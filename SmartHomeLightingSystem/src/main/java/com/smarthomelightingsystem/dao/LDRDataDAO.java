fd602beaf7216107547cb7934ed2745b664cff8f
s2207966 <a.shynkar@student.utwente.nl>
2020-10-29 15:54:40 +0100

    Edited LDRDataDAO, LDRDataController

--- SmartHomeLightingSystem/src/main/java/com/smarthomelightingsystem/dao/LDRDataDAO.java
+++ SmartHomeLightingSystem/src/main/java/com/smarthomelightingsystem/dao/LDRDataDAO.java
@@ -3,45 +3,92 @@ package com.smarthomelightingsystem.dao;
 
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
+
 import com.smarthomelightingsystem.data.Database;
 import com.smarthomelightingsystem.model.LDRData;
 
 public class LDRDataDAO {
 	private static final String TABLENAME = "ldr_data";
+	
+	public void fill(ResultSet r, LDRData out) throws SQLException {
+		
+		String str = r.getString("timestamp");
+		String label = str.substring(13, 15);
+		
+		out.getData().add(r.getInt("data"));
+		out.getLabels().add(label);
 
-    public LDRData getDefault(boolean mobile) { // last 8h | mobile false: every h; mobile true: every 2h
+    }
+
+    public LDRData getDefault() { 
+        LDRData out = new LDRData();
     	Database DB = new Database();
     	try {
-    		if (mobile == true) {
-    			String q = "SELECT * "
-    			+ "FROM ldr_data_day "
-    			+ "WHERE ";
-    		}else if(mobile!= true) {
-    			String q= "SELECT * FROM ldr_data_day;";
-    		}
-    	}catch (Exception e) {
-			e.printStackTrace();
-		} finally{
-			DB.close();
-		}
-        return null;
+    		String q = "SELECT * FROM ldr_data_day "
+    				+ "LIMIT 8;";
+    		
+        	PreparedStatement prepStatement = DB.connection.prepareStatement(q);
+        	ResultSet r = DB.executePreparedStatement(prepStatement);
+        	
+        	while (r.next()) { fill(r, out); }
+        	
+    	} catch (SQLException e) { e.printStackTrace(); } 
+    	finally { DB.close(); }
+    	
+    	return out;
+			
     }
 
-    public LDRData getDay(boolean mobile) { // mobile false: every 2h; mobile true: every 4h
-        return null;
+    public LDRData getDay() {
+    	LDRData out = new LDRData();
+    	Database DB = new Database();
+    	try {
+    		String q = "SELECT * FROM ldr_data_day";
+    		
+    		PreparedStatement prepStatement = DB.connection.prepareStatement(q);
+        	ResultSet r = DB.executePreparedStatement(prepStatement);
+        	
+        	while(r.next()) {fill(r, out);}
+        	
+    	} catch (SQLException e) {e.printStackTrace(); } 
+    	finally{ DB.close(); }
+    	
+    	return out;
     }
 
-    public LDRData getWeek() { // every day regardless of mobile
-        return null;
+    public LDRData getWeek() {
+    	LDRData out = new LDRData();
+    	Database DB = new Database();
+    	try {
+    		String q = "SELECT * FROM ldr_data_week";
+    		
+    		PreparedStatement prepStatement = DB.connection.prepareStatement(q);
+    		ResultSet r = DB.executePreparedStatement(prepStatement);
+        	
+        	while(r.next()) {fill(r, out);}
+        	
+    	} catch (SQLException e) {e.printStackTrace(); } 
+    	finally{ DB.close(); }
+    	
+    	return out;
     }
 
-    public LDRData getMonth(boolean mobile) { // mobile false: every 2d; mobile true: every 4d
-
-        return null;
-
+    public LDRData getMonth() { 
+    	LDRData out = new LDRData();
+    	Database DB = new Database();
+    	try {
+    		String q = "SELECT * FROM ldr_data_month";
+    		
+    		PreparedStatement prepStatement = DB.connection.prepareStatement(q);
+    		ResultSet r = DB.executePreparedStatement(prepStatement);
+    	} catch (SQLException e) {e.printStackTrace(); } 
+    	finally{ DB.close(); }
+    	
+    	return out;
     }
 
     public void setValue(float value) {
@@ -50,7 +97,6 @@ public class LDRDataDAO {
     		String q = "INSERT INTO " + TABLENAME + "(data, timestamp) VALUES(?, ?);" ;
     		PreparedStatement prepStatement = DB.connection.prepareStatement(q);
 			prepStatement.setFloat(1, value);
-			//LocalDateTime localDateTime = m.getTime().toInstant().atZone(ZoneId.of("GMT+0")).toLocalDateTime();
 			Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 			prepStatement.setTimestamp(2, timestamp);
 			

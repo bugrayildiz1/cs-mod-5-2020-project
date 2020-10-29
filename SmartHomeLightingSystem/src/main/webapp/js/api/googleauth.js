9e21fd7d8f8f59780487f00dd57177c1f1a395b9
jvanamerongen2002 <57096003+jvanamerongen2002@users.noreply.github.com>
2020-10-29 16:44:32 +0100

    Added sign out feature

--- SmartHomeLightingSystem/src/main/webapp/js/api/googleauth.js
+++ SmartHomeLightingSystem/src/main/webapp/js/api/googleauth.js
@@ -40,6 +40,11 @@ function onSignIn() {
 
     loadCurrentSetup();
 
+    $(".shls-settings-profile-dialog .mdc-dialog__content").html(`
+        <p>Name: ${GPROFILE.getName()}</p>
+        <p>Email: ${GPROFILE.getEmail()}</p>
+    `);
+
     if (SETUP.p === 0 || SETUP.q === 0) {
         openWelcome();
         openSetUp();

81d2ddfa9aa85d47b22c57fc2a0af2f93d054530
Jasper van Amerongen <j.vanamerongen-1@student.utwente.nl>
2021-02-11 16:32:32 +0100

    Reshaped and updated the project structure and dependencies for my own benefit.

--- SmartHomeLightingSystem/src/main/java/com/smarthomelightingsystem/exceptions/IllegalLDRDataScopeException.java
+++
@@ -1,9 +0,0 @@
-package com.smarthomelightingsystem.exceptions;
-
-public class IllegalLDRDataScopeException extends SHLSException {
-
-    public IllegalLDRDataScopeException() {
-        super("The provided scope of the LDR Data is not defined!");
-    }
-
-}
--- SmartHomeLightingSystem/src/main/java/com/smarthomelightingsystem/exceptions/IllegalSetupException.java
+++
@@ -1,9 +0,0 @@
-package com.smarthomelightingsystem.exceptions;
-
-public class IllegalSetupException extends SHLSException {
-
-    public IllegalSetupException() {
-        super("The provided setup is not defined or interfering!");
-    }
-
-}
